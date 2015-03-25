7a9a00037af20273122c022bb8777f2d30868fea
class <class@CST_N417_09.cst.citytech.cuny.edu>
2015-03-25 17:04:31 -0400

    Changed While loop to For loop

--- src/cst1201/CharacterCounter.java
+++ src/cst1201/CharacterCounter.java
@@ -27,11 +27,12 @@ public class CharacterCounter {
 
         System.out.println("Your character was: " + inputCharacter);
 
-        int i = 0; //the index of the current character in inputString.
+        
         int characterCount = 0; //the number of characters found.
 
         //traverse each character in the string.
-        while (i < inputString.length()) {
+        for ( int i = 0; i < inputString.length(); i++ ) {
+        //while (i < inputString.length()) {
             //if the current character matches the input character.
             if (inputString.charAt(i) == inputCharacter) {
                 characterCount++; //increment a count.
@@ -41,9 +42,7 @@ public class CharacterCounter {
                 //occurrences.
                 //For homework, due Wednesday evening.
             }
-
-            //move to the next character.
-            i++;
+                      
         }
 
         //output the final count. 
